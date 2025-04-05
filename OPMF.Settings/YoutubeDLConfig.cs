4995ce1aa4b9a99088e5b16049c1b0048e345d59
Junying Chen <jygchen@gmail.com>
2025-04-05 18:10:34 +1100

    Handle yt-dlp normal messages

--- OPMF.Settings/YoutubeDLConfig.cs
+++ OPMF.Settings/YoutubeDLConfig.cs
@@ -7,5 +7,9 @@
         public string VideoExtension { get; set; } = "mp4";
         public string SubtitleExtension { get; set; } = "vtt";
         public int MaxNoOfParallelDownloads { get; set; } = 5;
+        public string[] FalseErrorMessages { get; set; } =
+        [
+            "If you know what you are doing and want only the best pre-merged format, use \"-f b\" instead to suppress this warning",
+        ];
     }
 }
